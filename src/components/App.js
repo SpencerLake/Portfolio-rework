4041f276d253468fc336cf575542fe971000aaef
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-12-22 08:18:24 -0500

    started rework

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Landing from './Landing.js'
+import About from './About.js'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Landing/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
